28c3308ab08803c1b52e16cb9bf1d42ef83d0341
Andrey Khomenko <xomaczar@gmail.com>
2015-04-01 00:39:05 +0000

    upgraded to ember-cli@0.2.2

--- addon/ember-ion-rangeslider.js
+++ addon/ember-ion-rangeslider.js
@@ -1,10 +1,7 @@
 import Ember from 'ember';
 import SliderMixin from './mixins/ion-slider';
 
-var get = Ember.get,
-    set = Ember.set,
-    isArray = Ember.isArray,
-    isNone = Ember.isNone;
+var get = Ember.get;
 
 export default Ember.Component.extend(SliderMixin, {
   tagName: 'input',
@@ -27,25 +24,24 @@ export default Ember.Component.extend(SliderMixin, {
       options.onChange = Ember.run.bind(this, '_sliderDidChange', throttleTimeout);
       options.onFinish = Ember.K;
     }
-
-    var oneWayOptions = this.get('ionOneWayProperties');
-    return Ember.merge(options, oneWayOptions);
+		
+		return options;
+    //var oneWayOptions = this.get('ionOneWayProperties');
+    //return Ember.merge(options, oneWayOptions);
   }).readOnly(),
 
   //## Setup/destroy
   setupRangeSlider: function(){
     var options = get(this, 'sliderOptions');
-    console.log("Got options: ", options);
+    //console.log("Got options: ", options);
     this.$().ionRangeSlider(options);
     this.slider = this.$().data('ionRangeSlider');
     this.startObservingOneWayProps();
-
   }.on('didInsertElement'),
 
   destroyRangeSlider: function(){
     this.stopObservingOneWayProps();
     this.slider.destroy();
-
   }.on('willDestroyElement'),
 
   //## Bound values observers
@@ -58,7 +54,6 @@ export default Ember.Component.extend(SliderMixin, {
       //## In case where multiple sliders bound to the same property
       //## don't update the active slider values (to/from) as it results in a
       //## a loss of focus in a currently active slider
-      console.log("onToFromValuesChanged")
       if(!this.slider.is_active){
         this.slider.update(this.getProperties(propName));
       }
